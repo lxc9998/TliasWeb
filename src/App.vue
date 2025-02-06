0e915b9ae690e3b0cf732782272c6889e16db973
李飞飞 <11051283+biophilla@user.noreply.gitee.com>
2025-02-06 22:16:23 +0800

    1、剔除vue3demo内容 2、为什么数据、函数、展示都写了但还是没有响应式？

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+// 数据
+let name = '张三'
+let age = 18
+
+// 函数
+function addName() {
+  return name += 1
+}
+function addAge() {
+  return age += 1
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>响应式</h1>
+  <div>姓名：{{ name }}</div>
+  <div>年龄：{{ age }}</div>
+  <!-- Emmt快速创建标签 button[@click="addName"]{修改姓名} -->
+  <button @click="addName">修改姓名</button>
+  <button @click="addAge">年龄加1</button>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
