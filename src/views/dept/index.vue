53b203804e25959cf40013c3fbdf260e141558d0
李飞飞 <18850481939@139.com>
2025-03-29 00:10:22 +0800

    0329-新增编辑删除安南

--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -7,7 +7,13 @@ const deptList = ref([])
 const queryAll = async () =>{
   const result = await queryAllApi()
   deptList.value = result.data
-  console.log(deptList.value)
+  console.log(result.data)
+}
+const editDept = (id:number) =>{
+  console.log('部门id:', id)
+}
+const deleteDept = (id:number) =>{
+  console.log('部门id:',id)
 }
 
 onMounted(()=>{
@@ -16,14 +22,19 @@ onMounted(()=>{
 </script>
 
 <template>
-  <el-table :data="deptList">
-    <el-table-column type="index" label="序号" />
+  <el-table :data="deptList" :border="true">
+    <el-table-column type="index" label="序号" :min-width="20"/>
     <el-table-column prop="name" label="部门名称" />
     <el-table-column prop="createTime" label="创建时间" />
     <el-table-column prop="updateTime" label="更新时间"  />
+    <el-table-column label="操作">
+      <template #default="scope">
+        <el-button type="primary" size="small" @click="editDept(scope.row.id)">编辑</el-button>
+        <el-button type="danger" size="small" @click="deleteDept(scope.row.id)">删除</el-button>
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
 <style scoped>
-
 </style>
