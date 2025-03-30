3786e649e2da9c8e8cd1a16d4c89aa878d32c3af
李飞飞 <18850481939@139.com>
2025-03-30 23:06:17 +0800

    0330-实现新增部门表单校验

--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref, reactive } from 'vue'
+import { onMounted, ref } from 'vue'
 import {queryAllApi,addDeptApi} from '@/api/dept'
 import { ElMessage } from 'element-plus'
 
@@ -18,12 +18,14 @@ const formTitle = ref()
 // 表单数据
 const deptForm = ref({deptName:''})   // Element-plus中的model属性必须绑定为对象,哪怕只有一个string类型的属性
 // 表单校验规则
-const rules = reactive({
+const rules = ref({
   deptName: [
     { required: true, message: '请输入部门名称', trigger: 'blur' }, // trigger: 'blur'输入框失去焦点时触发校验规则
     { min: 2, max: 12, message: '部门名称2-12字符', trigger: 'blur' },
   ]
 })
+
+const deptFormRef = ref(null)
 
 
 // 打开新增部门对话框
@@ -34,24 +36,30 @@ const openAddDeptDialog = () =>{
   deptForm.value = {deptName:''}
   // 修改表单标题
   formTitle.value = '新增部门'
-  // 清空表单校验规则
-
+  // 重置表单校验规则
+  if (deptFormRef.value) {  // 如果表单已经渲染,则清空表单校验规则
+    deptFormRef.value.resetFields()
+  }
 }
 
-// 新增部门
+// 新增部门请求
 const submitDept = async () =>{
-  // 请求新增部门接口
-  const result = await addDeptApi(deptForm.value);  // deptForm.value目前就是一个对象:{name:'输入的部门名称'}
-  if (result.code) {  // code提示:类型“AxiosResponse<any, any>”上不存在属性“code”先不管,不好解决
-    // 提示新增成功
-    ElMessage.success('新增部门：'+deptForm.value.deptName+'成功！');
-  } else {
-    ElMessage.error('操作失败：'+result.msg);
-  }
-  // 关闭对话框
-  showDialog.value = false;
-  // 刷新部门列表
-  queryAll();
+  if(!deptFormRef.value) return;  // vue3中的ref会在组件挂载后才会被赋值,防止表单未渲染就调用.validate等方法报错
+  await deptFormRef.value.validate(async(valid)=>{
+    if (valid) {  // 表单校验通过
+      // 请求新增部门接口
+      const result = await addDeptApi(deptForm.value);  // deptForm.value目前就是一个对象:{name:'输入的部门名称'}
+      if (result.code) {  // code提示:类型“AxiosResponse<any, any>”上不存在属性“code”先不管,不好解决
+        ElMessage.success('新增部门：'+deptForm.value.deptName+' 成功！'); // 提示新增成功
+      } else {  // 提示新增失败
+        ElMessage.error('操作失败：'+result.msg);
+      }
+      showDialog.value = false; // 关闭对话框
+      await queryAll(); // 刷新部门列表
+    }else{  // 表单校验不通过
+      ElMessage.error('操作失败：请检查输入的部门名称是否符合要求！');
+    }
+  })
 }
 
 const editDept = (id:number) =>{
@@ -72,15 +80,15 @@ onMounted(()=>{
 
   <!-- 新增/编辑对话框 -->
   <el-dialog v-model="showDialog" :title="formTitle" width="500" :close-on-click-modal="false">
-    <el-form :model="deptForm" :rules="rules">  <!-- :model="deptForm"表示这个表单的数据绑定到这个对象上 -->
-      <el-form-item label="部门名称" props="deptName">
+    <el-form :model="deptForm" :rules="rules" ref="deptFormRef">  <!-- :model="deptForm"表示这个表单的数据绑定到这个对象上; :rules="rules"表示将检验规则与该表单进行绑定-->
+      <el-form-item label="部门名称" prop="deptName">  <!-- prop="deptName"表示使用rules中的deptName规则 -->
         <el-input v-model="deptForm.deptName"/>  <!-- 注意要修改的是表单中的deptName字段,请求的时候要带这个字段,所以是双向绑定 -->
       </el-form-item>
     </el-form>
     <template #footer>
       <div class="dialog-footer">
         <el-button @click="showDialog=!showDialog">取消</el-button>
-        <el-button type="primary" @click="submitDept">确认</el-button>
+        <el-button type="primary" @click="submitDept()">确认</el-button>
       </div>
     </template>
   </el-dialog>
