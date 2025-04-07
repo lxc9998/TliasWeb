842430999bbeb57a39598df30e3871a58302ae9f
李飞飞 <18850481939@139.com>
2025-04-07 23:09:40 +0800

    0407-完成员工列表;1.列表新增选择框2.增加自定义模板结合v-if3.居中显示字段内容

--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -125,11 +125,11 @@ onMounted(()=>{
 
   <!-- 部门列表 -->
   <el-table :data="deptList" :border="true">
-    <el-table-column type="index" label="序号" :min-width="20"/>
-    <el-table-column prop="deptName" label="部门名称" />
-    <el-table-column prop="createTime" label="创建时间" />
-    <el-table-column prop="updateTime" label="更新时间"  />
-    <el-table-column label="操作">
+    <el-table-column type="index" label="序号" :min-width="20" align="center"/>
+    <el-table-column prop="deptName" label="部门名称" align="center"/>
+    <el-table-column prop="createTime" label="创建时间" align="center"/>
+    <el-table-column prop="updateTime" label="更新时间"  align="center"/>
+    <el-table-column label="操作" align="center">
       <template #default="scope">
         <el-button type="primary" size="small" @click="editDept(scope.row.id)">编辑</el-button>
         <el-button type="danger" size="small" @click="deleteDept(scope.row.id)">删除</el-button>
--- src/views/emp/index.vue
+++ src/views/emp/index.vue
@@ -13,6 +13,23 @@ watch(()=>searchForm.value.date,(newVal)=>{   // ()=>searchForm.value.date表示
   }
 })
 
+const empList = ref([       
+  {
+    "id": 1,
+    "username": "jinyong",
+    "name": "金庸",
+    "gender": 1,
+    "image": "https://web-framework.oss-cn-hangzhou.aliyuncs.com/2022-09-02-00-27-53B.jpg",
+    "job": 2,
+    "salary": 8000,
+    "entryDate": "2015-01-01",
+    "deptId": 2,
+    "deptName": "教研部",
+    "createTime": "2022-09-01T23:06:30",
+    "updateTime": "2022-09-02T00:29:04"
+  }]
+)
+
 const search = () =>{
   console.log(searchForm.value);  // 1、可以看到date是一个数组,但给后端是两个date值,所以要用新的字段传给后端
 }
@@ -24,7 +41,7 @@ const clear = () =>{
 
 <template>
   {{ searchForm }}  <!-- 为方便看对象的值,可以通过{{}}直接输出方便查看;也可以通过vue开发工具修改响应式数据 -->
-  <!-- 行内表单 -->
+  <!-- 搜索区域(行内表单) -->
   <el-form :inline="true" :model="searchForm" class="demo-form-inline">
     <el-form-item label="姓名">
       <el-input v-model="searchForm.name" placeholder="请输入姓名" clearable />
@@ -43,6 +60,41 @@ const clear = () =>{
       <el-button type="danger" @click="clear">重置</el-button>
     </el-form-item>
   </el-form>
+
+  <!-- 员工列表1、列表新增选择框2、自定义模板3、v-if -->
+  <el-table :data="empList" border style="width: 100%">
+    <el-table-column type="selection" align="center"/>
+    <el-table-column prop="name" label="姓名" align="center"/>
+    <el-table-column prop="gender" label="性别" align="center">
+      <template #default="scope">
+        {{ scope.row.gender == 1 ? '男' : '女'}}
+      </template>
+    </el-table-column>
+    <el-table-column prop="image" label="头像" align="center">
+      <template #default="scope">
+        <img :src="scope.row.image" height="40">
+      </template>
+    </el-table-column>
+    <el-table-column prop="deptName" label="所属部门" align="center"/>
+    <el-table-column prop="job" label="职位" align="center">
+      <template #default="scope">
+        <span v-if="scope.row.job == 1">班主任</span>
+        <span v-else-if="scope.row.job == 2">讲师</span>
+        <span v-else-if="scope.row.job == 3">学工主管</span>
+        <span v-else-if="scope.row.job == 4">教研主管</span>
+        <span v-else-if="scope.row.job == 5">咨询师</span>
+        <span v-else>其他</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="entryDate" label="入职日期" align="center"/>
+    <el-table-column prop="updateTime" label="最后操作时间" align="center"/>
+    <el-table-column label="操作" align="center">
+      <template #default="scope">
+        <el-button type="primary" size="mini" @click="">编辑</el-button>
+        <el-button type="danger" size="mini" @click="">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <style scoped>
